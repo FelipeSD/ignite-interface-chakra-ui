d704561c6c4398b21931a84b0901623849da298b
Felipe Damasceno <felipesdam@outlook.com>
2021-12-18 17:17:58 -0300

    banner home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,27 @@
 import { Box, Button, Flex, Icon, Image } from "@chakra-ui/react";
 import { ChevronLeftIcon } from '@chakra-ui/icons';
 
-export function Header() {
+type HeaderProps = {
+    hasBackButton?: boolean;
+    onBackButtonClick?: () => void;
+}
+
+export function Header({ hasBackButton, onBackButtonClick }: HeaderProps) {
     return (
         <Box bg="gray.50" p={5}>
             <Flex
                 as="header"
                 mx="auto"
                 position="relative"
-                align="center" 
+                align="center"
                 maxW="container.lg">
-                <Button size="xs" position="absolute"  p="0" variant='ghost'>
-                    <ChevronLeftIcon boxSize="6" />
-                </Button>
+                    
+                {hasBackButton && (
+                    <Button size="xs" position="absolute" p="0" variant='ghost'>
+                        <ChevronLeftIcon boxSize="6" />
+                    </Button>
+                )}
+
                 <Image mx="auto" src="logo.png" alt="Logo" />
             </Flex>
         </Box>
